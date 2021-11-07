bb96e353146fb0c16d4c702d51c8bd9143ecd835
Resilient Coder <resilientcoders@Resilients-MBP.cable.rcn.com>
2021-11-06 21:55:53 -0400

    sat

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+
+const dbName = "instagram";
+
+module.exports = {
+  url: "mongodb+srv://UrjiH:getpassword@cluster0.rh8ci.mongodb.net/insta?retryWrites=true&w=majority",
+};
